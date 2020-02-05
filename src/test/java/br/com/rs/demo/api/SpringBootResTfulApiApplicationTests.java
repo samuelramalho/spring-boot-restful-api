15425d8cf1dd0708ce53a0f4059366688875b523
samuel.ramalho <samuel.ramalho@ans.gov.br>
2020-02-05 16:23:59 -0300

    Cria projeto

---
+++ src/test/java/br/com/rs/demo/api/SpringBootResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rs.demo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
