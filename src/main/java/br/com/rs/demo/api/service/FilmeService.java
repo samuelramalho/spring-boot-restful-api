374aa67c80fca51f811c4973c519f77d8e39f35b
samuel.ramalho <samuel.ramalho@ans.gov.br>
2020-02-10 15:23:21 -0300

    Implementa service layer

---
+++ src/main/java/br/com/rs/demo/api/service/FilmeService.java
@@ -0,0 +1,36 @@
+package br.com.rs.demo.api.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import br.com.rs.demo.api.domain.Filme;
+import br.com.rs.demo.api.repository.FilmeRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class FilmeService {
+	
+	@Autowired
+	private FilmeRepository repository;
+
+	public Page<Filme> findAll(Integer ano, Pageable pageable){
+        return (ano == null) ? repository.findAll(pageable) : repository.findByAno(ano, pageable);
+    }
+
+	public Optional<Filme> findById(Long id) {
+		return repository.findById(id);
+	}
+    
+    public Filme save(Filme article) {
+		return repository.save(article);
+	}
+    
+    public void deleteById(Long id) {
+    	repository.deleteById(id);
+	}
+}
